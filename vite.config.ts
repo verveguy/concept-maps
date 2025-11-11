553cf2477f0dec1c26b251b9f17eef5c4881dbe1
Brett Adam <brett.adam@autodesk.com>
2025-11-10 21:18:54 -0700

    Run test in github action. Code coverage reports

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,21 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
     css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html', 'lcov'],
+      exclude: [
+        'node_modules/',
+        'src/test/',
+        '**/*.d.ts',
+        '**/*.config.*',
+        '**/dist/',
+        '**/build/',
+        '**/.github/',
+        '**/docs/',
+        '**/scripts/',
+        '**/public/',
+      ],
+    },
   },
 })
